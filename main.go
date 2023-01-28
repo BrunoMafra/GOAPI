905b2b8ea06fc544821dbee1d612a0edb5fa774f
Bruno Mafra <brunomp2714@gmail.com>
2023-01-28 19:47:59 -0300

    added connection with db (docker)

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 var temp = template.Must(template.ParseGlob("./templates/*.html"))
 
+func connectDB() *sql.DB {
+	conn := "user=postgres dbname=store password=admin host=localhost port=3001 sslmode=disable"
+	db, err := sql.Open("postgres", conn)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Product struct {
+	Id          int
 	Name        string
 	Description string
 	Price       float64
@@ -21,11 +35,36 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	products := []Product{
-		{"Xbox One", "Good for gaming", 349, 5},
-		{"PS5", "Good for exclusives", 400, 3},
-		{"Nintendo", "Mario", 200, 7},
-		{"Gaming PC", "GTX 4090", 1099, 6},
+	conn := connectDB()
+	selectProducts, err := conn.Query("select * from products")
+
+	if err != nil {
+		panic(err.Error())
 	}
+
+	p := Product{}
+	products := []Product{}
+
+	for selectProducts.Next() {
+		var Id, Quant int
+		var Name, Description string
+		var Price float64
+
+		err := selectProducts.Scan(&Id, &Name, &Description, &Price, &Quant)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = Id
+		p.Name = Name
+		p.Description = Description
+		p.Price = Price
+		p.Quant = Quant
+
+		products = append(products, p)
+	}
+
 	temp.ExecuteTemplate(w, "Index", products)
+	defer conn.Close()
 }
